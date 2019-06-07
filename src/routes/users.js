ba3544a47f5a0df245e2c99d1af159336bfed026
Jose Navarro <jmn8718@gmail.com>
2019-06-07 12:49:59 +0200

    extract consentUrl

--- src/routes/users.js
+++ src/routes/users.js
@@ -9,6 +9,8 @@ router.get('/', ensureLoggedIn('/login'), function(req, res, next) {
     userString: JSON.stringify(req.user),
     user: req.user,
     sessionString: JSON.stringify(req.session),
+    consentUrl:
+      '/dialog/authorize?response_type=code&client_id=id2019&scope=email%20op&redirect_uri=http://localhost:8080/users/consent',
   };
 
   res.render('users', pageOptions);
@@ -17,16 +19,16 @@ router.get('/', ensureLoggedIn('/login'), function(req, res, next) {
 router.get('/consent', ensureLoggedIn('/login'), function(req, res, next) {
   const { code } = req.query;
   if (code) {
-  const pageOptions = {
-    title: 'Users',
-    isAuthenticated: true,
-    user: req.user,
+    const pageOptions = {
+      title: 'Users',
+      isAuthenticated: true,
+      user: req.user,
       code,
       clientId: 'id2019',
       clientSecret: 'secret2019',
       redirectUri: 'http://localhost:8080/users/consent',
       grantType: 'authorization_code',
-  };
+    };
 
     res.render('exchange', pageOptions);
   } else {
