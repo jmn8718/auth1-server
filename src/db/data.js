3c7cac085a038e00762345199262e166a1748bd9
Jose Navarro <jmn8718@gmail.com>
2019-06-10 15:58:44 +0200

    extract seed data

---
+++ src/db/data.js
@@ -0,0 +1,18 @@
+const CLIENTS = [
+  {
+    name: 'client demo',
+    clientId: 'id2019',
+    clientSecret: 'secret2019',
+    redirectUri: 'http://localhost:8080/users/consent',
+  },
+  {
+    name: 'auth app',
+    clientId: 'auth19id',
+    clientSecret: 'auth10secret',
+    redirectUri: 'http://localhost:8080/users/consent',
+  },
+];
+
+module.exports = {
+  CLIENTS,
+};
--- src/db/seeds/clients.js
+++ src/db/seeds/clients.js
@@ -1,6 +1,7 @@
 const { forEach } = require('lodash');
 const { Client } = require('../client');
 const { logger } = require('../../logger');
+const { CLIENTS } = require('../data');
 
 async function generateClient(clientData) {
   logger.debug(`Generating client => ${JSON.stringify(clientData)}`);
@@ -12,21 +13,6 @@ async function generateClient(clientData) {
   logger.debug(`CLIENT created => ${JSON.stringify(client)}`);
   return client;
 }
-
-const CLIENTS = [
-  {
-    name: 'client demo',
-    clientId: 'id2019',
-    clientSecret: 'secret2019',
-    redirectUri: 'http://localhost:8080/users/consent',
-  },
-  {
-    name: 'auth app',
-    clientId: 'auth19id',
-    clientSecret: 'auth10secret',
-    redirectUri: 'http://localhost:8080/users/consent',
-  },
-];
 
 async function generateClients() {
   logger.debug('Generating clients seeds...');
